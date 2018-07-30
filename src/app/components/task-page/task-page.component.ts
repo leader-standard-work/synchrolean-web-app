54dc169bacc74cb5200e203fedf142d3506e9792
Decklan <decklan007@gmail.com>
2018-07-29 19:01:47 -0700

    Changed task service to use a BehaviorSubject and return that as an observable. Moved add button on task page.

--- src/app/components/task-page/task-page.component.ts
+++ src/app/components/task-page/task-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '../../../../node_modules/@angular/router';
 
 import { TaskService } from './../../services/task.service';
@@ -9,7 +9,7 @@ import { Task } from '../../models/Task';
   templateUrl: './task-page.component.html',
   styleUrls: ['./task-page.component.css']
 })
-export class TaskPageComponent implements OnInit {
+export class TaskPageComponent implements OnInit, OnDestroy {
   public pageTitle: string = 'My Tasks';  // Page title
   public tasks: Task[] = [];              // List of tasks from service
   public currentIndex: number = 0;        // The index of the currently referenced task from the list
@@ -31,46 +31,29 @@ export class TaskPageComponent implements OnInit {
   }
 
   /**
-   * Method for paying attention to which element in the list is currently
-   * selected.
-   * @param event Event emitter bound to the tasklist component in the view
-   */
-  change(event) {
-    this.currentIndex = event;
-  }
-
-  /**
-   * Get the current user's completed tasks
-   */
-  getCompletedTasks() {
-    console.log('TaskPageComponent: Getting completed tasks');
-    this.tasks = this.taskService.getCompletedTasks();
-  }
-
-  /**
-   * Get the current user's unfinished tasks
-   */
-  getUnfinishedTasks() {
-    console.log('TaskPageComponent: Getting unfinished tasks');
-    this.tasks = this.taskService.getUnfinishedTasks();
-  }
-
-  /**
    * Get all tasks for the current user
    */
   getAllTasks() {
     console.log('TaskPageComponent: Getting all tasks');
-    this.tasks = this.taskService.getAllTasks();
+    this.taskService.getAllTasks()
+      .subscribe((tasks) => {
+        this.tasks = tasks;
+      }, (err) => { console.log(err) });
   }
 
   /**
    * Navigate to the add task form. This will likely change in location.
    */
   addTask() {
+    this.taskService.updateObservableState(this.tasks);
     this.router.navigate(['/addtask']);
   }
 
   editTask(index: number) {
     console.log(index);
+  }
+
+  ngOnDestroy() {
+
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Task } from '../models/Task';
 import { environment } from '../../environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,16 @@ export class TaskService {
    * Production version of this url should be in src/environments/environment.prod.ts.
    */
   private apiBase: string = '/tasks/'; // Api addition to the url
-  private tasks:Task[] = [];           // Local collection of tasks
+  private tasksSubject: BehaviorSubject<Task[]>;
+  private tasksObservable: Observable<Task[]>;
 
   constructor(private http: HttpClient,
   private authService: AuthService) { 
     console.log('TaskService created.'); // For logging purposes
+    this.tasksSubject = new BehaviorSubject([]);
+    this.tasksObservable = this.tasksSubject.asObservable();
     if (this.authService.isCurrentUser()) {
-      this.fetchTasks(this.authService.getCurrentUserId());
+      this.tasksObservable = this.fetchTasks(this.authService.getCurrentUserId());
     }
   }
 
@@ -29,28 +33,10 @@ export class TaskService {
    * @param id The id of the user to retrieve tasks for
    * @returns  A list of tasks belonging to the user matching id arg
    */
-  fetchTasks(id:number) {
+  fetchTasks(id:number): Observable<Task[]> {
     const endpoint = environment.baseServerUrl + this.apiBase + id;
     console.log('TaskService: Fetching tasks from server');
-    this.http.get<Task[]>(endpoint, { withCredentials: true })
-      .subscribe(data => {
-        // We will also want to make sure we grab data appropriately
-        data.forEach(task => {
-          let newTask = new Task();
-          newTask.id = task.id;
-          newTask.ownerId = task.ownerId;
-          newTask.name = task.name;
-          newTask.description = task.description;
-          newTask.isCompleted = task.isCompleted;
-          newTask.isRecurring = task.isRecurring;
-          newTask.isRemoved = task.isRemoved;
-          newTask.completionDate = task.completionDate;
-          newTask.creationDate = task.creationDate;
-          this.tasks.push(newTask);
-        })
-      }, err => {
-        console.log(err);
-      });
+    return this.http.get<Task[]>(endpoint);
   }
 
   /**
@@ -58,29 +44,9 @@ export class TaskService {
    * the component that needs the tasks.
    * @returns List of all tasks for the current user.
    */
-  getAllTasks() {
+  getAllTasks(): Observable<Task[]> {
     console.log('TaskService: Getting all tasks');
-    return this.tasks;
-  }
-
-  /**
-   * Get completed tasks for the current user
-   * @returns List of completed tasks for the current user
-   */
-  getCompletedTasks() {
-    console.log('TaskService: Getting completed tasks');
-    let completedTasks: Task[] = this.tasks.filter(task => task.isCompleted === true);
-    return completedTasks;
-  }
-
-  /**
-   * Get unfinished tasks for the current user
-   * @returns List of unfinished tasks for the user
-   */
-  getUnfinishedTasks() {
-    console.log('TaskService: Getting unfinished tasks');
-    let unfinishedTasks: Task[] = this.tasks.filter(task => task.isCompleted === false);
-    return unfinishedTasks;
+    return this.tasksObservable;
   }
 
   /**
@@ -90,20 +56,27 @@ export class TaskService {
    */
   addTask(newTask: Task) {
     const endpoint = environment.baseServerUrl + this.apiBase;
-    this.http.post(endpoint, newTask)
-      .subscribe((newTask:Task) => this.tasks.push(newTask));
+    return this.http.post(endpoint, newTask)
+      // .subscribe((newTask:Task) => this.tasks.push(newTask));
   }
 
   /**
-   * Updates a task's information in the database.
-   * @param updatedTask The updated task whose changes will be saved to the database
-   * @returns       Returns the newly updated task back to the client
+   * Updates the state of the observable array of tasks
    */
-  editTask(index: number, updatedTask: Task) {
-    let id = this.authService.getCurrentUserId();
-    const endpoint = environment.baseServerUrl + this.apiBase + id + '/' + updatedTask.id;
-    this.tasks.splice(index, 1);
-    this.http.put(endpoint, updatedTask)
-      .subscribe((task:Task) => this.tasks.push(task));
+  updateObservableState(tasks: Task[]) {
+    this.tasksSubject.next(tasks);
   }
+
+  // /**
+  //  * Updates a task's information in the database.
+  //  * @param updatedTask The updated task whose changes will be saved to the database
+  //  * @returns       Returns the newly updated task back to the client
+  //  */
+  // editTask(index: number, updatedTask: Task) {
+  //   let id = this.authService.getCurrentUserId();
+  //   const endpoint = environment.baseServerUrl + this.apiBase + id + '/' + updatedTask.id;
+  //   this.tasks.splice(index, 1);
+  //   this.http.put(endpoint, updatedTask)
+  //     .subscribe((task:Task) => this.tasks.push(task));
+  // }
 }
