77313c8786d761bb6b7e49be28abbb72f73e268e
Decklan <decklan007@gmail.com>
2018-08-06 16:17:40 -0700

    Fixed issue in auth guard that was occuring when checking the decoded JWT (Added <any> type).

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 import * as JWT from 'jwt-decode';
 
 @Injectable({
@@ -7,20 +8,22 @@ import * as JWT from 'jwt-decode';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private authService: AuthService) { }
 
+  // Route guard
   canActivate(): boolean {
     let token = localStorage.getItem('jwt');
-    let decoded = JWT(token);
+    let decoded = <any>JWT(token);
     let currentTime = Date.now() / 1000;
 
-    console.log(currentTime < decoded.exp);
-
-    // Should also check expiration somehow
+    // Check to make sure the token is there and that the token hasn't expired
     if (token && currentTime < decoded.exp) {
       return true;
     }
 
+    // If the token has expired log the user out and route them to the home page
+    this.authService.logout();
     this.router.navigate(['/home']);
     return false;
   }
