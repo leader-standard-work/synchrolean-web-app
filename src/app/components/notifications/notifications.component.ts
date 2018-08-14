0bc7a8d0e9958556796c023fc094fd59cf001edc
Decklan <decklan007@gmail.com>
2018-08-14 01:40:15 -0700

    Started working on notifications component and making invites to teams look nicer.

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,10 +1,9 @@
-import { Team } from './../../models/Team';
-import { AuthService } from './../../services/auth.service';
+import { AccountService } from './../../services/account.service';
 import { TeamService } from './../../services/team.service';
 import { Component, OnInit } from '@angular/core';
 import { AddUserRequest } from '../../models/AddUserRequest';
 import { Router } from '../../../../node_modules/@angular/router';
-import { Observable } from 'rxjs';
+import { Account } from '../../models/Account';
 
 @Component({
   selector: 'app-notifications',
@@ -13,18 +12,32 @@ import { Observable } from 'rxjs';
 })
 export class NotificationsComponent implements OnInit {
   public invites: AddUserRequest[];
+  public inviteeAccounts: Account[] = [];
+  public inviterName: string;
 
   constructor(
     private teamService: TeamService,
-    private authService: AuthService,
+    private accountsService: AccountService,
     private router: Router) { }
 
   ngOnInit() {
     // Grab the notifications from the team service
     this.teamService.fetchTeamInvites()
       .subscribe((invites) => { 
-        this.invites = invites 
+        this.invites = invites;
+        this.accountsService.getAccountByEmail(this.invites[0].inviterEmail)
+        .subscribe((inviter) => {
+          this.inviteeAccounts.push(inviter);
+          this.inviterName = this.inviteeName();
+        }, (err) => { console.log(err) });
       }, (err) => { console.log(err) });
+  }
+
+  /**
+   * Returns the name of the person inviting the user to a team
+   */
+  inviteeName() {
+    return `${this.inviteeAccounts[0].firstName} ${this.inviteeAccounts[0].lastName}`;
   }
 
   acceptTeamInvite(invite: AddUserRequest) {
