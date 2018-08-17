d3550f4e979176a86bc43f31c2a8ec0949621ab6
Decklan <decklan007@gmail.com>
2018-08-16 18:35:08 -0700

    For some reason it said AddUserRequest was deleted even though it was on the repo. Had to delete it, commit, then re-add it.

---
+++ src/app/models/AddUserRequest.ts
@@ -0,0 +1,13 @@
+/**
+ * A basic model for an AddUserRequest. We can change this as needed. For now
+ * it is just set up like this for test purposes (getting and displaying
+ * AddUserRequests).
+ */
+export class AddUserRequest {
+    inviteeEmail: string;
+    inviterEmail: string;
+    teamId: number;
+    isAuthorized: boolean;
+    
+    constructor() {}
+}
