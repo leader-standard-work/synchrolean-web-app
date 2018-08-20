5b59902d552431f1c67f741cf019058f4430ed54
Decklan <decklan007@gmail.com>
2018-08-19 20:37:04 -0700

    Pulled latest.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import { Router } from '@angular/router';
-import { AuthService } from './../../services/auth.service';
-import { Account } from './../../models/Account';
-import { Component, OnInit } from '@angular/core';
-=======
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
->>>>>>> 7e16d066e4495d4b0226cad0ab17ea8daf4401b9
 
 @Component({
   selector: 'sidebar',
