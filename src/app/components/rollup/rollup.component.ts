6c5c415d84423a6a7ccc26021068a41067e48cf8
Decklan <decklan007@gmail.com>
2018-08-20 01:07:21 -0700

    Cleaned up the rollup component and added some functionality. View entire rollup list or view by individual team member.

--- src/app/components/rollup/rollup.component.ts
+++ src/app/components/rollup/rollup.component.ts
@@ -1,7 +1,9 @@
+import { Team } from './../../models/Team';
 import { Task } from '../../models/Task';
 import { TeamService } from '../../services/team.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Account } from './../../models/Account';
 
 @Component({
   selector: 'app-rollup',
@@ -10,7 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RollupComponent implements OnInit {
   private teamId: number;
+  private team: Team;
+  public teamMembers: Account[];
   public rollupTasks: Task[];
+  public userTasks: Task[];
+
+  public complete: string = 'Done';
+  public uncomplete: string = 'In Progress';
+  public showAllTasks: boolean = true;
 
   constructor(private route: ActivatedRoute,
     private teamService: TeamService) {
@@ -25,6 +34,42 @@ export class RollupComponent implements OnInit {
       .subscribe((rollup) => {
         this.rollupTasks = rollup;
       }, err => console.log(err));
+
+    this.teamService.getTeam(this.teamId)
+      .subscribe((team) => {
+        this.team = team;
+        this.teamService.fetchTeamMembers(this.team.id)
+          .subscribe((members) => {
+            this.teamMembers = members;
+          }, (err) => { console.log(err) });
+      }, (err) => { console.log(err) });
+  }
+
+  /**
+   * Filters the list of tasks by the task's ownerEmail 
+   * @param email The email of the person whose tasks we want to filter by
+   */
+  selectUserTasks(email: string) {
+    this.userTasks = [];
+    for (let task of this.rollupTasks) {
+      if (task.ownerEmail === email)
+        this.userTasks.push(task);
+    }
+    this.showIndividual();
+  }
+
+  /**
+   * Shows all team tasks in one list when true
+   */
+  showAll() {
+    this.showAllTasks = true;
+  }
+
+  /**
+   * Flag when showing individual user's tasks
+   */
+  showIndividual() {
+    this.showAllTasks = false;
   }
 
 }
